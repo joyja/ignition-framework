e65b28d72f1a48bd6b39e18ca8e5278a4166b578
Ubuntu <ubuntu@ignition1.lxd>
2021-01-11 18:17:54 +0000

    initial commit

---
+++ ignition/script-python/theme/code.py
@@ -0,0 +1,23 @@
+def getBackgroundClass(param):
+	paramParts = param.split()
+	color = paramParts[0]
+	if len(paramParts) > 1:
+		modifier = paramParts[1]
+		return 'color/%s/%s' % (color, modifier)
+	else:
+		if color == 'black' or color == 'white':
+			return 'color/%s' % (color,)
+		else:
+			return 'color/%s/base' % (color,)
+			
+def getVariable(param):
+	paramParts = param.split()
+	color = paramParts[0]
+	if len(paramParts) > 1:
+		return '--%s' % (param.replace(' ','-'),)
+	else:
+		if color == 'black' or color == 'white':
+			return '--%s' % (color,)
+		else:
+			return '--%s' % (param.replace(' ','-'),) + '-base'		
+	
